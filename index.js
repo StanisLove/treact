a6b8d5f138379cc197e73861a5c0cb9931a191c5
Stanislav Kravchenko <stankra@yandex.ru>
2019-10-22 09:02:34 +0300

    Events

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import fetchRepos from '~/src/github';
+import Counter from '~/src/Counter';
+import Person from '~/src/Person';
+import EventListener from '~/src/EventListener';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { repos: [] };
-  }
-
-  componentDidMount() {
-    fetchRepos()
-      .then((repos) => {
-        this.setState({ repos }); // { repos: repos }
-      })
-  }
-
-  render() {
-    const { repos } = this.state;
-
-    return (
-      <div>
-        <ul>
-          {
-            repos.map((repo) => (
-              <li>{repo}</li>
-            ))
-          }
-        </ul>
-      </div>
-    );
-  }
-}
 
 ReactDOM.render(
-  <App />,
+  <div>
+    <Counter value={1} />
+    <Person />
+    <EventListener />
+  </div>,
   document.getElementById('root')
 );
