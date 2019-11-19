2d3006acd0afba19174a3a2f002e6251ca32e5f7
Stanislav Kravchenko <stankra@yandex.ru>
2019-11-19 09:37:58 +0300

    Add request for Products

---
+++ apiConstants.js
@@ -0,0 +1,4 @@
+export const apiUrl = "https://cdn.contentful.com";
+export const spaceId = "zya20mm9grsa";
+export const accessToken = "IbNr7vESQaVBIcoTyORm9RBiQ2K9bXLNDrwAAA22ytU";
+export const environmentId = "master";
--- src/components/views/Catalog/index.js
+++ src/components/views/Catalog/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const CatalogPage = () => (
-  <p>Products</p>
-);
+import request from 'superagent';
+
+import { apiUrl, spaceId, accessToken, environmentId } from '~/apiConstants';
+
+class CatalogPage extends Component {
+  constructor(props) {
+    super(props);
+    this.stat = { entries: [] };
+  }
+
+  componentDidMount() {
+    request
+      .get(`${apiUrl}/spaces/${spaceId}/environments/${environmentId}/entries`)
+      .query({'content_type': 'product'})
+      .set('Authorization', `Bearer ${accessToken}`)
+      .then((res) => console.log(res.body));
+  }
+
+  render() {
+    return(
+      <p>Products</p>
+    );
+  }
+}
 
 export default CatalogPage;
-
