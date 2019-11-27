b36f3fb2a98b621958ebc52578b4afdd807c3ebb
Stanislav Kravchenko <stankra@yandex.ru>
2019-11-27 18:25:34 +0300

    init [0:57]

---
+++ App.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+
+class childApp extends Component {
+  constructor(props) {
+    console.log('child', 'constructor');
+    super(props);
+    this.state = { stateProp: 'newValue' };
+  }
+
+  componentDidMount() {
+    console.log('child', 'componentDidMount');
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('child', 'getSnapshotBeforeUpdate', prevProps, prevState);
+    return null;
+  }
+
+  getDerivedStateFromProps(props, state) {
+    console.log('child', 'getDerivedStateFromProps', props, state);
+    return state;
+  }
+
+  shouldComponentUpdate() {
+    console.log('child', this.shouldComponentUpdate);
+    return true;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('child', 'componentDidUpdate', prevProps, prevState, snapshot);
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    console.log('constructor');
+    super(props);
+    this.state = { stateProp: 'oldValue' };
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('getSnapshotBeforeUpdate', prevProps, prevState);
+    return null;
+  }
+
+  getDerivedStateFromProps(props, state) {
+    console.log('getDerivedStateFromProps', props, state);
+    return state;
+  }
+
+  shouldComponentUpdate() {
+    console.log(this.shouldComponentUpdate);
+    return true;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('componentDidUpdate', prevProps, prevState, snapshot);
+  }
+}
+  render() {
+    console.log('render');
+    return (
+      <div>
+        <button onClick={() =>this.setState({ stateProp: 'newValue' }) } />
+        { /* <ChildApp stateProp={this.state.stateProp} /> */ }
+      </div>
+    );
+  }
+}
--- index.js
+++ index.js
@@ -1,37 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import fetchRepos from '~/src/github';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { repos: [] };
-  }
-
-  componentDidMount() {
-    fetchRepos()
-      .then((repos) => {
-        this.setState({ repos }); // { repos: repos }
-      })
-  }
-
-  render() {
-    const { repos } = this.state;
-
-    return (
-      <div>
-        <ul>
-          {
-            repos.map((repo) => (
-              <li>{repo}</li>
-            ))
-          }
-        </ul>
-      </div>
-    );
-  }
-}
+import App from './App';
 
 ReactDOM.render(
   <App />,
